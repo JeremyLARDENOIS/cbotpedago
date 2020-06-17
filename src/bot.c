8b314f0e512a44065ac7ca9efdd2ecc05c67a330
Sebastien Druon <seb@homedruon.com>
2020-06-17 23:48:02 +0200

    pploup

--- src/bot.c
+++ src/bot.c
@@ -40,7 +40,7 @@ int callback(struct ld_context *context, enum ld_callback_reason reason, void *d
         ld_json_message_init(&message);
         ld_json_pack_message(&message, (json_t *) data);
        
-        sprintf( message_seb, "Je te vois taper %p", message.author ) ;
+        sprintf( message_seb, " mentions %p\n reactions %p", message.mentions, message.reactions ) ;
 
         ld_send_basic_message(context, message.channel_id, message_seb );
 
